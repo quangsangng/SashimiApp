3ba8941ef66075ed91a791ccb32e3bdc414c1a62
Sang Cá Mập <55171560+sangcamap@users.noreply.github.com>
2022-12-27 17:18:27 +0700

    thêm list views và search bar

---
+++ SashimiApp/SashimiApp/Models/LibraryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SashimiApp.Models
+{
+    public class LibraryItem
+    {
+        /* 
+            Một Library Item gồm có
+            ID
+            Content: Từ khóa (kanji, từ vựng)
+            Explain: Giải thích nghĩa của từ đó
+            Example: Ví dụ sử dụng từ đó
+        */
+        public int ID { get; set; } 
+        public string Content { get; set; }
+        public string Explain { get; set; }
+        public string Example_1 { get; set; }
+        public string Example_2 { get; set; }
+    }
+}
